ed26670509db5d838375a7a5b2abff6389dcb5a7
PhungHuuPhu <phunghuuphu2004@gmail.com>
2025-11-23 10:13:00 +0700

    .

--- DALTUDXD_LOPNV90_2025_030287/DALTUDXD_LOPNV90_2025_030287/View/MainView.xaml.cs
+++ DALTUDXD_LOPNV90_2025_030287/DALTUDXD_LOPNV90_2025_030287/View/MainView.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -38,5 +40,42 @@ namespace DALTUDXD_LOPNV90_2025_030287.View
         {
 
         }
+
+        private void cbtengach_DropDownOpened(object sender, EventArgs e)
+        {
+            cbtengach.ItemsSource = new List<string>
+            {
+              "Gạch men, gạch gốm bán sứ ( Ceramic tile )",
+              "Gạch đá hoa cương ( Granite tile )",
+              "Gạch xương sứ, bóng kiếng ( Porcelain tile )",
+              "Gạch đất nung ( brick tile )",
+              "Gạch bông ( Cement tile )",
+              "Gạch đá hạt mài ( Terrazzo )",
+              "Gạch đá cẩm thạch ( Marble tile )",
+              "Gạch kính ( Glass tile )",
+              "Gạch giả gỗ hoặc gỗ tự nhiên ( Wood tile )",
+              "Gạch đá tự nhiên ( Natural Stone tile )",
+              "Gạch sợi nén ( Laminate tile )",
+              "Gạch Nhựa ( Vinyl tile )",
+              "Ván sàn tre, trúc ( Bamboo tile )",
+              "Sàn gỗ bần ( Cork tile )",
+              "Gạch thảm mô-đun ( carpet tile )",
+              "Sàn cao su ( Rubber tile )",
+              "Sàn Epoxy"
+            };
+        }
+        private void cbKichThuoc_DropDownOpened(object sender, EventArgs e)
+        {
+            cbKichThuoc.ItemsSource = new List<string>
+            {
+              "300x300",
+              "400x400",
+              "600x600",
+              "800x800",
+              "1200x600"
+    };
+        }
+
+
     }
 }
